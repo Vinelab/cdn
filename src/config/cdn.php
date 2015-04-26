79b0c9edb72155ee348a9a81ae8d203376595c50
olimorris <olimorris@users.noreply.github.com>
2015-04-26 14:34:48 +0100

    Added support for S3 with CloudFront as a CDN

--- src/config/cdn.php
+++ src/config/cdn.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'bypass' => $app->environment('local') ? false : false,
+    'bypass' => false,
 
     /*
     |--------------------------------------------------------------------------
@@ -34,10 +34,10 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | S3 URL
+    | CDN URL
     |--------------------------------------------------------------------------
     |
-    | Set your S3 url, [without your bucket name]
+    | Set your CDN url, [without the bucket name]
     |
     */
     'url' => 'https://s3.amazonaws.com',
@@ -73,8 +73,8 @@ return [
             's3' => [
 
                 'credentials' => [
-                    'key'       => env('S3_ACCESS_KEY'),
-                    'secret'    => env('S3_SECRET_KEY'),
+                    'key'       => '',
+                    'secret'    => '',
                 ],
 
                 /*
@@ -92,7 +92,7 @@ return [
                 |
                 */
                 'buckets' => [
-                    'spareskills' => '*',
+                    'bucket-name' => '*',
                     //        'your-js-bucket-name-here'   =>  ['public/js'],
                     //        'your-css-bucket-name-here'  =>  ['public/css'],
                 ],
@@ -108,21 +108,7 @@ return [
                 | predefined grants: private, public-read, public-read-write, authenticated-read
                 | bucket-owner-read, bucket-owner-full-control, log-delivery-write
                 */
-                'acl' => 'public-read',
-
-                /*
-                |--------------------------------------------------------------------------
-                | Use CloudFront as the CDN
-                |--------------------------------------------------------------------------
-                |
-                | Amazon S3 can be linked to CloudFront through distributions. This allows
-                | the files in your S3 buckets to be served from a number of global
-                | locations to achieve low latency and faster page load times.
-                */
-                'cloudfront' => [
-                    'use'       => false,
-                    'cdn_url'   => '',
-                ],
+                'acl' => 'public-read'
 
             ],
 
